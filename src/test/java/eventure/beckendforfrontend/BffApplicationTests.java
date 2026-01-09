6265c618056ce5fd274a8bcde3c82eda9639f6c4
Bovsunovskydima <bovsdmitrij1515@gmail.com>
2026-01-09 14:55:52 +0200

    setup bff

---
+++ src/test/java/eventure/beckendforfrontend/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package eventure.beckendforfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
